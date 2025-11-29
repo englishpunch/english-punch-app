1b567f8bdcac1d63af22b717e0e1255918c4a332
Kim Tae Hoon <eszqsc112@gmail.com>
2025-11-29 12:58:13 +0900

    기능 추가: knip 설정 파일 추가 및 ESLint 관련 패키지 업데이트, SignOutButton 컴포넌트 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import reactCompiler from "babel-plugin-react-compiler";
 
 const host = process.env.TAURI_DEV_HOST;
 
@@ -9,7 +10,7 @@ export default defineConfig(async () => ({
   plugins: [
     react({
       babel: {
-        plugins: ["babel-plugin-react-compiler"],
+        plugins: [reactCompiler],
       },
     }),
     tailwindcss(),
