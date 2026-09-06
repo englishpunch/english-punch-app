3fe87425ab3288f97fc3fbb20c30630590e2779b
Kim Tae Hoon <eszqsc112@gmail.com>
2026-09-06 18:23:15 +0900

    feat: refine activity history and compact tables #91

--- src/lib/dayjs.ts
+++ src/lib/dayjs.ts
@@ -8,6 +8,5 @@ configuredDayjs.extend(timezone);
 export const DATE_FORMAT = "YYYY-MM-DD";
 export const DATETIME_FORMAT = "YYYY-MM-DD HH:mm";
 export const TIME_FORMAT = "HH:mm";
-export const WEEKDAY_FORMAT = "ddd";
 
 export const dayjs = configuredDayjs;
